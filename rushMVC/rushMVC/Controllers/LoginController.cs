17d4f4e359e5b05b6ab6bc7af104b21d32d04fe4
iesplin123 <Igan34@hotmail.com>
2017-08-03 22:40:59 -0600

    Adding Login Page

---
+++ rushMVC/rushMVC/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using rushMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rushMVC.Controllers
+{
+    public class LoginController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (Model1 db = new Model1())
+                {
+                    var obj = db.Users.Where(a => a.User_Name.Equals(objUser.User_Name) && a.Password.Equals(objUser.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.User_ID.ToString();
+                        Session["UserName"] = obj.User_Name.ToString();
+                        return RedirectToAction("About");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
